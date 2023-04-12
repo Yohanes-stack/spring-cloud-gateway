66b6f5f9fdb11c12f4af75d1b1a27f1900e7a21b
Yohanes <Y0hanes@outlook.com>
2023-04-12 17:13:04 +0800

    修改配置，增加route-service服务 方便调试

---
+++ spring-cloud-route-service/src/main/java/com.route/controller/RouteController.java
@@ -0,0 +1,12 @@
+package com.route.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RouteController {
+	@RequestMapping("/test")
+	public String test(){
+		return "success";
+	}
+}
